f934ce59a118b1ca175492e3b6c3ef1db680ab7b
oieusouweverton <oieusouweverton@hotmail.com>
2018-08-07 21:18:56 -0300

    Adição do Botão de Scan do QRCode. Startando Intent da Camera de Scan, e recebendo os resultados. Valores recebidos são mostrados em um alert.

--- app/src/main/java/br/edu/univasf/escolaverde/pevcredence/TelaCredenciamento.java
+++ app/src/main/java/br/edu/univasf/escolaverde/pevcredence/TelaCredenciamento.java
@@ -1,12 +1,20 @@
 package br.edu.univasf.escolaverde.pevcredence;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class TelaCredenciamento extends DebugActivity {
-
+    Button scanButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +25,41 @@ public class TelaCredenciamento extends DebugActivity {
         getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
         getSupportActionBar().setTitle(title);
 
+        scanButton = (Button) findViewById(R.id.btScan);
+        final Activity activity = this; //salvando a activity atual para passar como parametro no IntentIntegrator
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator itIntegrator = new IntentIntegrator(activity); //Instanciando integrator
+                itIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES); // setando o tipo de leitura para qrcode
+                itIntegrator.setPrompt("QRCode Scan"); //Nome que será mostrado na tela da camera
+                itIntegrator.setCameraId(0); //Qual camera será usada 0-traseira 1-frontal
+                itIntegrator.initiateScan(); //Iniciando o scan
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Deve-se implementar esse método para receber os dados.
+        //IntentResult recebe a resposta do integrador
+        IntentResult itResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        //
+        if(itResult != null){
+            if(itResult.getContents() != null){
+                alert(itResult.getContents());
+            }else{
+                alert("Scan cancelado pelo usuário!");
+            }
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void alert(String str){
+        Toast.makeText(getContext(), str, Toast.LENGTH_SHORT).show();
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -29,4 +72,7 @@ public class TelaCredenciamento extends DebugActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public Context getContext() {
+        return this;
+    }
 }
